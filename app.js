3a6ec1bba080731482fa39a438fc27f49c8e4282
Isis-Yamel <isis.salamanca@globant.com>
2020-12-28 23:05:25 -0500

    feat: refactor existing functionaly [generateTiles] and add compare methods to getFacts

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function Specie(data) {
 // Create Dino Objects
 const dinoFactory = (dino) => {
     return dino.map((dino) => {
-        return new Specie(dino)
+        return new Specie(dino);
     });
 };
 
@@ -26,64 +26,91 @@ const user = new Specie({
     weight: '',
     diet: '',
     height: '',
-    fact: ''
+    fact: '',
 });
 
 // Use IIFE to get human data from form
-
-const getHumanData = (function() {
-    document.getElementById('btn').addEventListener('click', function(e) {
-        replaceContent();
-        user.name = document.getElementById('name').value;
-        user.height = (parseFloat(document.getElementById('feet').value) * 12) + parseFloat(document.getElementById('inches').value);
-        user.weight = parseFloat(document.getElementById('weight').value);
-        user.diet = document.getElementById('diet').value;
-        createTiles(createdDinos);
-    });
+const getHumanData = (function () {
+  document.getElementById('btn').addEventListener('click', function (e) {
+    replaceContent();
+    user.name = document.getElementById('name').value;
+    user.height =
+      parseFloat(document.getElementById('feet').value) * 12 +
+      parseFloat(document.getElementById('inches').value);
+    user.weight = parseFloat(document.getElementById('weight').value);
+    user.diet = document.getElementById('diet').value;
+    createTiles(createdDinos);
+  });
 })();
 
+// *NOTE: Weight in JSON file is in lbs, height in inches.
 // Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
+const compareHeight = item => item.fact = item.height > 63 ? `${item.species}, you are like a Anklyosaurus height!` : `${item.species}'s height is about 6 pigeons`;
 
 // Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
+const compareWeight = item => item.fact = item.weight > 100 ? `${item.species} you are equivalent to 700 Brachiosaurus` : `${item.species} are you a feather?`;
 
 // Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
+const compareOriginalFact = item => item.fact;
+
+//Obtain random fact
+const getFact = item => {
+    const randomKey = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+    switch (randomKey) {
+        case 0:
+            return compareHeight(item)
+            break;
+        case 1:
+            return compareWeight(item)
+            break;
+        case 2:
+           return compareOriginalFact(item)
+            break;
+        case 3:
+            return compareWeight(item)
+            break;
+        default:
+            break;
+    };
+};
+
+//Generate random order for dinos
+const randomGenerator = random => random.sort(() => Math.random() - 0.5);
+
+//Validate to show name when specie is Human
+const getName = item => item.species === 'Human' ? item.name : item.species;
 
 // Generate Tiles for each Dino in Array
-
 const createTiles = (createdDinos) => {
-    createdDinos.splice(3, 0, user);
-    let tiles = document.getElementById('grid');
-    tiles.innerHTML = "";
+    randomGenerator(createdDinos);
+    createdDinos.splice(4, 0, user);
+    let content = '';
 
-    createdDinos.forEach(item => {
-        const tile = document.createElement('div');
-        tile.className = 'grid-item';
+    createdDinos.forEach((item) => {
+        let name = getName(item);
+        let fact = getFact(item);
 
-        const title = document.createElement('h2');
-        title.textContent = item.species;
-
-        const image = document.createElement('img');
-        image.src = `images/${item.species}.png`
-
-        const fact = document.createElement('p');
-        fact.textContent = item.fact;
-
-        tile.appendChild(title);
-        tile.appendChild(image);
-        tile.appendChild(fact);
-
-        tiles.appendChild(tile)
+        content +=
+        `<div class="grid-item">
+            <h2>${name}</h2>
+            <img src="images/${item.species}.png"/>
+            <p>${fact}</p>
+        </div>`
     });
+
+    buildTiles(content);
 };
 
 // Add tiles to DOM
+const buildTiles = content => {
+    let tiles = document.getElementById('grid');
+    tiles.innerHTML = content;
+};
 
 // Remove form from screen
 const replaceContent = () => {
-    document.getElementById('dino-compare').style.display = "none";
+    document.getElementById('dino-compare').style.display = 'none';
 };
 
 // On button click, prepare and display infographic
